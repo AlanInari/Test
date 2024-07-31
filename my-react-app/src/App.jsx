c9df565858e4eb2cfdc4f7d34847af0a4e1ed7aa
AlanInari <veterandgo@gmail.com>
2024-07-30 20:52:19 -0600

    componente nuevo
    en este commit se adicionan los cambios necesarios para empezar un
    componente desde cero, he adjuntado dos imagenes para comenzar a
    modificarlas y saber sus alcances

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import girls_banner from './assets/girls_banner.png';
+import cocoa_idol from './assets/cocoa_idol.webp'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <img src={girls_banner} alt="girls_banner" className="girls-banner"/>
+      <img src={cocoa_idol} alt="cocoa_banner" className="cocoa-idol"/>
+    </div>
   )
 }
 
